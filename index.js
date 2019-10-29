12d36244ea8533262433b1c5c4ce4cff1597c014
David Hérault <dherault@gmail.com>
2019-10-29 16:15:46 +0100

    Add offset angle

--- index.js
+++ index.js
@@ -12,57 +12,70 @@ const translationVector = { x: width / 2, y: height / 2 }
 
 const hexahedron = createHexahedron()
 
+let da = 0
+let db = 0
+let dc = 0
+
 document.getElementById('button-a-plus').onclick = () => {
-  hexahedron.rotation.a += Math.PI / 8
-  draw()
+  da += Math.PI / 240
 }
 document.getElementById('button-a-minus').onclick = () => {
-  hexahedron.rotation.a -= Math.PI / 8
-  draw()
+  da -= Math.PI / 240
 }
 document.getElementById('button-b-plus').onclick = () => {
-  hexahedron.rotation.b += Math.PI / 8
-  draw()
+  db += Math.PI / 240
 }
 document.getElementById('button-b-minus').onclick = () => {
-  hexahedron.rotation.b -= Math.PI / 8
-  draw()
+  db -= Math.PI / 240
 }
 document.getElementById('button-c-plus').onclick = () => {
-  hexahedron.rotation.c += Math.PI / 8
-  draw()
+  dc += Math.PI / 240
 }
 document.getElementById('button-c-minus').onclick = () => {
-  hexahedron.rotation.c -= Math.PI / 8
-  draw()
+  dc -= Math.PI / 240
+}
+
+document.getElementById('button-alpha-plus').onclick = () => {
+  hexahedron.rotationOffset.alpha += Math.PI / 16
+}
+document.getElementById('button-alpha-minus').onclick = () => {
+  hexahedron.rotationOffset.alpha -= Math.PI / 16
+}
+document.getElementById('button-beta-plus').onclick = () => {
+  hexahedron.rotationOffset.beta += Math.PI / 16
+}
+document.getElementById('button-beta-minus').onclick = () => {
+  hexahedron.rotationOffset.beta -= Math.PI / 16
+}
+document.getElementById('button-gamma-plus').onclick = () => {
+  hexahedron.rotationOffset.gamma += Math.PI / 16
+}
+document.getElementById('button-gamma-minus').onclick = () => {
+  hexahedron.rotationOffset.gamma -= Math.PI / 16
 }
 
 function draw() {
-  console.log('draw')
   _.fillRect(0, 0, width, height)
 
   const points = hexahedron.nodes
     .map(node => scaleNode(node, 200))
-    .map(node => projectOnZPlane(node, hexahedron.rotation))
+    .map(node => projectOnZPlane(node, hexahedron.rotation, hexahedron.rotationOffset))
     .map(point => translateOnZPlane(point, translationVector))
 
-  console.log('points', points)
-  points.forEach(drawPoint)
-
+  _.beginPath()
   hexahedron.vertices.forEach(([i, j]) => {
-    _.beginPath()
     _.moveTo(points[i].x, points[i].y)
     _.lineTo(points[j].x, points[j].y)
-    _.closePath()
-    _.stroke()
   })
-}
-
-function drawPoint({ x, y }) {
-  _.beginPath()
-  _.arc(x, y, 3, 0, 2 * Math.PI)
   _.closePath()
   _.stroke()
+}
+
+function update() {
+  hexahedron.rotation.a += da
+  hexahedron.rotation.b += db
+  hexahedron.rotation.c += dc
+  draw()
 }
 
 function multiplyMatrices(a, b) {
@@ -83,13 +96,19 @@ function multiplyMatrices(a, b) {
   return c
 }
 
-function projectOnZPlane({ x, y, z }, { a, b, c }) {
+function projectOnZPlane({ x, y, z }, { a, b, c }, { alpha, beta, gamma }) {
   const ca = cos(a)
   const sa = sin(a)
   const cb = cos(b)
   const sb = sin(b)
   const cc = cos(c)
   const sc = sin(c)
+  const calpha = cos(alpha)
+  const salpha = sin(alpha)
+  const cbeta = cos(beta)
+  const sbeta = sin(beta)
+  const cgamma = cos(gamma)
+  const sgamma = sin(gamma)
 
   const X = [[x], [y], [z]]
   const rotateX = [
@@ -107,9 +126,24 @@ function projectOnZPlane({ x, y, z }, { a, b, c }) {
     [sc, cc, 0],
     [0, 0, 1],
   ]
+  const rotateAlpha = [
+    [1, 0, 0],
+    [0, calpha, -salpha],
+    [0, salpha, calpha],
+  ]
+  const rotateBeta = [
+    [cbeta, 0, -sbeta],
+    [0, 1, 0],
+    [sbeta, 0, cbeta],
+  ]
+  const rotateGamma = [
+    [cgamma, -sgamma, 0],
+    [sgamma, cgamma, 0],
+    [0, 0, 1],
+  ]
 
-  const Y = multiplyMatrices(rotateZ, multiplyMatrices(rotateY, multiplyMatrices(rotateX, X)))
-
+  let Y = multiplyMatrices(rotateZ, multiplyMatrices(rotateY, multiplyMatrices(rotateX, X)))
+  Y = multiplyMatrices(rotateGamma, multiplyMatrices(rotateBeta, multiplyMatrices(rotateAlpha, Y)))
   // const projection = [
   //   [1, 0, 0.5],
   //   [0, 1, 0.5],
@@ -177,9 +211,12 @@ function createHexahedron() {
       [3, 7],
     ],
     rotation: { a: 0, b: 0, c: 0 },
+    rotationOffset: { alpha: 0, beta: 0, gamma: 0 },
   }
 
   return polyhedron
 }
 
-window.addEventListener('load', draw)
+window.addEventListener('load', () => {
+  setInterval(update, 20)
+})
